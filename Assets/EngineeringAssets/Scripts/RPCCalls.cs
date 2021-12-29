7ebbf0a3ed3f940fa5e6d29f74fec5ec2d134981
XtronSolutions <hr.xtronsolutions@gmail.com>
2021-12-29 05:18:25 +0500

    final fixes

--- Assets/EngineeringAssets/Scripts/RPCCalls.cs
+++ Assets/EngineeringAssets/Scripts/RPCCalls.cs
@@ -100,7 +100,7 @@ public class RPCCalls : MonoBehaviour
             }
             else
             {
-                Invoke("LoadAsyncScene", 3f);
+                MultiplayerManager.Instance.LoadSceneDelay();
             }
         }
         else
