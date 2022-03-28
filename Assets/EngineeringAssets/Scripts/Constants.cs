fce3369eab6314274a50140f398ddc981d013253
XtronSolutions <hr.xtronsolutions@gmail.com>
2022-03-29 02:21:52 +0500

    final fixes

--- Assets/EngineeringAssets/Scripts/Constants.cs
+++ Assets/EngineeringAssets/Scripts/Constants.cs
@@ -94,10 +94,10 @@ public static class Constants
     public static bool WalletChanged = false;
 
     public static bool IsTestNet = true;
-    public static bool IsTest = true;
+    public static bool IsTest = false;
     public static bool IsStagging = true;
-    public static bool DebugAllCars = true;// bool to enable all cars
-    public static bool IsDebugBuild = true;//apply pvp score 500+/enable force end race on levels
+    public static bool DebugAllCars = false;// bool to enable all cars
+    public static bool IsDebugBuild = false;//apply pvp score 500+/enable force end race on levels
 
     public static bool IsSendConfirmation = false;
     public static bool IsResetPassword = false;
